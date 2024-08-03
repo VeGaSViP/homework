04d4b7e068f87436d5a634abba2b6d5c1a93dac9
VeGaSViP <68062839+VeGaSViP@users.noreply.github.com>
2024-08-03 21:15:18 +0700

    lesson76

---
+++ lesson76/main.js
@@ -0,0 +1,23 @@
+const unmounted = 'const неизменяемая переменная'
+let changed = 'let изменяемая переменная'
+const firstName = 'Василий'
+const lastName = 'Пономарев'
+const ageMy = 29
+const nickname = 'Vegas'
+const profession = 'Web'
+
+console.log(unmounted)
+console.log(changed)
+console.log(firstName, lastName, ageMy, nickname, profession)
+
+let admin
+let name = 'Джон'
+admin = name
+console.log(admin)
+
+const ourPlanetName = 'Earth'
+const currentUserName = 'Name'
+
+const BIRTHDAY = '18.04.1982';
+
+const age = someCode(BIRTHDAY);
